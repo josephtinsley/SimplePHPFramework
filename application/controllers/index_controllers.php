9302f4ae872c10dc0357d3d299613c971982ebd3
Joseph Tinsley <joseph.tinsley@gmail.com>
2014-01-30 12:05:15 -0500

    Removing unused methods from the index controller

--- application/controllers/index_controllers.php
+++ application/controllers/index_controllers.php
@@ -11,28 +11,8 @@ Class index_controllers extends BaseController{
      BaseController::$view->_meta_description = 'Meta Description goes here';
      BaseController::$view->_meta_keywords = 'Page Keywords goes here';
      BaseController::$view->_greeting = 'Hello World! Welcome to our home page';
-     BaseController::$view->_profile_names = $this->show_profile_names(); 
-     BaseController::$view->_sample_form = $this->show_sample_form(); 
 
      BaseController::$view->display('index/index');   
     }
-    
-    private function show_profile_names() { 
-        BaseController::Model('about');
-        
-        $retVal = about_models::get_profile_names();
-        
-        $html ='<ul>'."\n";
-        for ($x = 0; $x < count($retVal); $x++)
-        {
-            $html .='<li>'.$retVal[$x]['f_name'] .':'.$retVal[$x]['l_name'].'</li>'."\n";
-        }
-        $html .='</ul>'."\n";
-        return $html;
-    }
-    
-    private function show_sample_form() {
-        require_once CLASSES.'ClassSampleForm.php';
-        return SampleForm::form();
-    }
+
 }//END CLASS
